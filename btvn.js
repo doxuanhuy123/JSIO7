41ba0e3d829c58e585b6e17b34669a537f47ff44
doxuanhuy123 <80506104+doxuanhuy123@users.noreply.github.com>
2021-11-13 20:20:34 +0700

    Add files via upload

--- btvn.js
+++ btvn.js
@@ -1,52 +1,65 @@
+
 bài1
-const arrayNumber = [10, -3, 5, -7, 11, -15, -20, 23];
-for (let i = 0; i < arrayNumber.length; i++ ) {
-    if (i % 2 == 0){
-        for ( let j = 0; j < arrayNumber[i].length; i++){
-            if (j % 2 != 0){
-                console.log(sum[arrayNumber[i][j]])
-            }
-        }
+class PhanSo {
+    constructor(t,m){
+        this.t = t
+        this.m = m
+    }
+add(phanso) {
+    return new PhanSo(this.t*phanso.m + phanso.t*this.m,this.m*phanso.m)
+}
+
+mul() {
+    return new PhanSo(this.t*phanso.m - phanso.t*this.m,this.m*phanso.m)
+}
+
+sub() {
+    return new PhanSo(this.t*phanso.t,this.m*phanso.m)
+}
+
+div() {
+    return new PhanSo(this.t*phanso.m,this.m*phanso.t)
+}
+
+show() {
+    console.log($(this.t)/$(this.m))
+}
+
+}
+    
+    
+    ____________________________
+Bài2
+const a = prompt("nhập số thí sinh: ")
+class Candidate {
+    constructor(id,name,dob,math,literature,english){
+        this.id = id
+        this.name = name
+        this.dob = dob
+        this.math = math
+        this.literature = literature
+        this.english = english
     }
 }
-____________
-const arrayNumber = [10, -3, 5, -7, 11, -15, -20, 23];
-const max = Math.max(...arrayNumber);
-console.log(max);
-_______________________
-const arrayNumber = [10, -3, 5, -7, 11, -15, -20, 23];
-const min = Math.min(...arrayNumber);
-console.log(min);
-_______________________
-const arrayNumber = [10, -3, 5, -7, 11, -15, -20, 23];
-const nguyenTo = arrayNumber.filter(myFunction);
-function myFunction(value) {
-    console.log(value % 2 != 0);
 
-bài2
-let mang = [1,34,45,647,5,25,87,59,90,10];
-mang.sort(function(a, b) {
-    return a - b;
-});
-console.log(mang);
-_______________________
-let mang = [1,34,45,647,5,25,87,59,90,10];
-mang.sort(function(a, b) {
-    return b-a;
-});
-console.log(mang);
-
-
-bài3
-let a = prompt("Nhập số");
-for (let i = 2; i < a; i+=2){
-
+const hs = []
+for ( let i = 0; i <= Number(a); i++) {
+    const student = new Candidate(prompt("id: "),prompt("name: "),prompt("dob: "),prompt("math: "),prompt("literature: "),prompt("english: "));
+    hs.push(student);
 }
-
-
-bài5
-let mang = [,1,2,3,4,5,,6,7,8,9,1,2,3,4,5,6,7,8,9];
-var thaySo = mang.filter(function(value){
-    return value % 3 == 0
-});
-console.log(mang);
+class TestCandidate {
+    run() {
+      for (let j = 0; j < hs.length; j++) {
+        if (
+          Number(hs[j].math) +
+            Number(hs[j].literature) +
+            Number(hs[j].english) >
+          15
+        ) {
+          console.log(hs[j]);
+        }
+      }
+    }
+  }
+  let test = new TestCandidate();
+  test.run();
